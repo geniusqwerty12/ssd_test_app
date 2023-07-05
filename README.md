93604c99d224dc78801bf611a685ab4d35bcf2ad
John Cristopher Mateo <johncristopher.mateo@wvsu.edu.ph>
2023-07-05 20:58:18 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tflite_model_tester
+ This simple python app lets you test tflite models on your computer, before deploying them to flutter apps
---
+++ main.py
@@ -0,0 +1,89 @@
+# Source
+# https://colab.research.google.com/github/EdjeElectronics/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi/blob/master/Train_TFLite2_Object_Detction_Model.ipynb#scrollTo=e4WtI8i5K96w
+import cv2
+import numpy as np
+from tensorflow.lite.python.interpreter import Interpreter
+
+# Load the Tensorflow Lite model into memory
+interpreter = Interpreter(model_path="model/detect.tflite")
+interpreter.allocate_tensors()
+
+# Get model details
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+height = input_details[0]['shape'][1]
+width = input_details[0]['shape'][2]
+
+float_input = (input_details[0]['dtype'] == np.float32)
+
+input_mean = 127.5
+input_std = 127.5
+
+# This determines the threshold for the predictions to be include
+# Any prediction above the min_conf value will be included in the output
+min_conf=0.3
+
+# Load the label map into memory
+with open("model/labelmap.txt", 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
+
+# define a video capture object
+vid = cv2.VideoCapture(0)
+  
+while(True):
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+
+    image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    imH, imW, _ = frame.shape 
+    image_resized = cv2.resize(image_rgb, (width, height))
+    input_data = np.expand_dims(image_resized, axis=0)
+    
+    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+    if float_input:
+        input_data = (np.float32(input_data) - input_mean) / input_std
+    
+    # Perform the actual detection by running the model with the image as input
+    interpreter.set_tensor(input_details[0]['index'],input_data)
+    interpreter.invoke()
+    
+    # Retrieve detection results
+    boxes = interpreter.get_tensor(output_details[1]['index'])[0] # Bounding box coordinates of detected objects
+    classes = interpreter.get_tensor(output_details[3]['index'])[0] # Class index of detected objects
+    scores = interpreter.get_tensor(output_details[0]['index'])[0] # Confidence of detected objects
+    
+    detections = []
+
+    # Loop over all detections and draw detection box if confidence is above minimum threshold
+    for i in range(len(scores)):
+        if ((scores[i] > min_conf) and (scores[i] <= 1.0)):
+
+            # Get bounding box coordinates and draw box
+            # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+            ymin = int(max(1,(boxes[i][0] * imH)))
+            xmin = int(max(1,(boxes[i][1] * imW)))
+            ymax = int(min(imH,(boxes[i][2] * imH)))
+            xmax = int(min(imW,(boxes[i][3] * imW)))
+            
+            cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+
+            # Draw label
+            object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+            label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+            label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+            cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+
+            detections.append([object_name, scores[i], xmin, ymin, xmax, ymax])
+    
+    cv2.imshow("Output", frame)
+        
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
